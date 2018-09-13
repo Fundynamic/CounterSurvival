521da102cabff4d958ab63dc6d0509308329e165
Stefan Hendriks <stefan@q42.nl>
2018-09-13 17:02:12 +0200

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    
+
+    public float moveSpeed;             //Floating point variable to store the player's movement speed.
+
+    private Rigidbody2D rb2d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
+
+    private float currentSpeed;
+
+    public float offset = 0.0f;
+
+    public GameObject light;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Get and store a reference to the Rigidbody2D component so that we can access it.
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        currentSpeed = 0.0f;
+        if (Input.GetButton("Horizontal") ||
+            Input.GetButton("Vertical"))
+        {
+            currentSpeed = moveSpeed;
+        }
+
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        difference.Normalize();
+
+        // Rotate player sprite
+        float rotation_z = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotation_z + offset);
+
+        // Rotate flashlight with it (let it point towards mouse)
+        float rotation_x = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        float rotation_y = Mathf.Atan2(difference.x, difference.y) * Mathf.Rad2Deg;
+
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = light.transform.position.z;
+        var targetRotation = Quaternion.LookRotation(Vector3.forward, (mousePos - transform.position));
+
+        //        light.transform.rotation = Quaternion.Euler(light.transform.position.x, light.transform.position.y, rotation_z + offset);
+        //        light.transform.rotation = Quaternion.Euler(light.transform.position.x, light.transform.position.y, rotation_z + offset);
+//        light.transform.rotation = targetRotation;
+//        light.transform.Rotate(0,0,90);
+    }
+
+    //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
+    void FixedUpdate()
+    {
+        //Store the current horizontal input in the float moveHorizontal.
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        //Store the current vertical input in the float moveVertical.
+        float moveVertical = Input.GetAxis("Vertical");
+
+        //Use the two store floats to create a new Vector2 variable movement.
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
+        //rb2d.AddForce(movement * speed);
+        var rb2DPosition = rb2d.position;
+        var newPosition = rb2DPosition + ((movement * currentSpeed) * Time.fixedDeltaTime);
+
+//        Debug.Log("Hor: " + moveHorizontal + " vert: " + moveVertical + " Speed: " + currentSpeed + " - current pos " + rb2DPosition + " - newPosition " + newPosition);
+
+        rb2d.MovePosition(newPosition);
+    }
+
+}
